ad01bff2ab144446e31b15e844fc36e0e33fbe18
Orlando Sabogal <o.sabogal10@uniandes.edu.co>
2019-02-04 22:47:39 -0500

    First commit

--- ColorBlindness/ViewController.swift
+++ ColorBlindness/ViewController.swift
@@ -8,13 +8,162 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func chooseImage(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                self.present(imagePickerController,animated: true,completion: nil)
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true,completion: nil)
+            }
+            else{
+                print("Camera not available")
+            }
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true,completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler:nil ))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    func processImage(in image:UIImage) -> UIImage? {
+        guard let cgimage = image.cgImage else{
+            print("error en CGImage")
+            return nil
+        }
+        
+        let colorSpace       = CGColorSpaceCreateDeviceRGB()
+        let width            = cgimage.width
+        let height           = cgimage.height
+        let bytesPerPixel    = 4
+        let bitsPerComponent = 8
+        let bytesPerRow      = bytesPerPixel * width
+        //let bitmapInfo = CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+        let bitmapInfo = RGBA32.bitmapInfo
+        
+        guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else{
+            print("error en context")
+            return nil
+        }
+        
+        context.draw(cgimage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        guard let buffer = context.data else {
+            print("unable to get context data")
+            return nil
+        }
+        
+        let pixelBuffer = buffer.bindMemory(to: RGBA32.self, capacity: width * height)
+        
+        for row in 0 ..< Int(height) {
+            for column in 0 ..< Int(width) {
+                let offset = row * width + column
+                let pixel = pixelBuffer[offset]
+                let pr = pixel.redComponent
+                let pg = pixel.greenComponent
+                let pb = pixel.blueComponent
+                let pa = pixel.alphaComponent
+                pixelBuffer[offset] = RGBA32(red: 0, green: pg, blue: pb, alpha: pa)
+//                if pixelBuffer[offset] == .black {
+//                    pixelBuffer[offset] = .red
+//                }
+            }
+        }
+        
+        let outputCGImage = context.makeImage()!
+        let outputImage = UIImage(cgImage: outputCGImage, scale: image.scale, orientation: image.imageOrientation)
+        
+        return outputImage
+        
+    }
+    
+    
+    @IBAction func applyFilter(_ sender: Any) {
+        
+        print("Filtrando")
+        let filteredImage = processImage(in: imageView.image!)
+        print("Filtrada")
+        imageView.image = filteredImage
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imageView.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    struct RGBA32: Equatable {
+        private var color: UInt32
+        
+        var redComponent: UInt8 {
+            return UInt8((color >> 24) & 255)
+        }
+        
+        var greenComponent: UInt8 {
+            return UInt8((color >> 16) & 255)
+        }
+        
+        var blueComponent: UInt8 {
+            return UInt8((color >> 8) & 255)
+        }
+        
+        var alphaComponent: UInt8 {
+            return UInt8((color >> 0) & 255)
+        }
+        
+        init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8) {
+            let red   = UInt32(red)
+            let green = UInt32(green)
+            let blue  = UInt32(blue)
+            let alpha = UInt32(alpha)
+            color = (red << 24) | (green << 16) | (blue << 8) | (alpha << 0)
+        }
+        
+        static let red     = RGBA32(red: 255, green: 0,   blue: 0,   alpha: 255)
+        static let green   = RGBA32(red: 0,   green: 255, blue: 0,   alpha: 255)
+        static let blue    = RGBA32(red: 0,   green: 0,   blue: 255, alpha: 255)
+        static let white   = RGBA32(red: 255, green: 255, blue: 255, alpha: 255)
+        static let black   = RGBA32(red: 0,   green: 0,   blue: 0,   alpha: 255)
+        static let magenta = RGBA32(red: 255, green: 0,   blue: 255, alpha: 255)
+        static let yellow  = RGBA32(red: 255, green: 255, blue: 0,   alpha: 255)
+        static let cyan    = RGBA32(red: 0,   green: 255, blue: 255, alpha: 255)
+        
+        static let bitmapInfo = CGImageAlphaInfo.premultipliedLast.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+        
+        static func ==(lhs: RGBA32, rhs: RGBA32) -> Bool {
+            return lhs.color == rhs.color
+        }
+    }
+    
 }
 
